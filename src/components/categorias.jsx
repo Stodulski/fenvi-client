891f37357b4b2416cd9c27e4ce0ceb38c7e7fb3f
Stodulski <maxisto043@gmail.com>
2024-10-24 18:26:05 -0300

    Cambios

--- src/components/categorias.jsx
+++ src/components/categorias.jsx
@@ -4,15 +4,15 @@ export const Categorias = () => {
   return (
     <section className="font-title mx-auto flex w-full max-w-5xl flex-col items-center justify-center pt-14 xs:gap-[20px] xs:pt-32">
       <div className="flex w-full flex-col xs:flex-row xs:gap-[20px]">
-        <article className="category flex h-72 w-full flex-col items-center animation__show justify-center bg-[url('esculturas.jpg')] bg-cover bg-center text-white text-shadow  md:h-96">
+        <article className="category animation__show flex h-72 w-full flex-col items-center justify-center bg-cover bg-center text-white text-shadow md:h-96">
           <h3 className="text-3xl font-bold md:text-4xl">Esculturas</h3>
         </article>
-        <article className="category flex h-72 w-full flex-col items-center bg__obras justify-center animation__show border-y-2 border-zinc-950 bg-[url('obras.jpg')] bg-cover bg-top text-white text-shadow hover:text-[#efb810] xs:border-none md:h-96">
+        <article className="category bg__obras animation__show flex h-72 w-full flex-col items-center justify-center border-y-2 border-[#856916] bg-cover bg-top text-white text-shadow hover:text-[#efb810] xs:border-none md:h-96">
           <h3 className="text-3xl font-bold md:text-4xl">Obras</h3>
         </article>
       </div>
 
-      <article className="category flex h-72 w-full flex-col items-center bg__deco animation__show justify-center bg-[url('deco.jpg')] bg-cover bg-center text-white text-shadow hover:text-[#efb810] md:h-96">
+      <article className="category bg__deco animation__show flex h-72 w-full flex-col items-center justify-center bg-cover bg-center text-white text-shadow hover:text-[#efb810] md:h-96">
         <h3 className="text-3xl font-bold md:text-4xl">Deco</h3>
       </article>
     </section>
