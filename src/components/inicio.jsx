4d9b0e319fef147ac671e7cf9077edd0fdbff32d
Stodulski <maxisto043@gmail.com>
2024-10-24 18:43:50 -0300

    Cambios

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Inicio = () => {
   return (
-    <section className='inicio before:contents(" ") flex h-[calc(100vh)] w-full flex-col items-center justify-center bg__home bg-cover bg-center px-5 text-center before:absolute before:bottom-0 before:h-32 before:w-full before:bg-gradient-to-t before:from-black before:to-transparent'>
+    <section className='inicio before:contents(" ") flex w-full flex-col items-center justify-center px-5 text-center before:absolute before:bottom-0 before:h-32 before:w-full before:bg-gradient-to-t before:from-black before:to-transparent'>
       <div className="flex w-full max-w-4xl flex-col items-center justify-center text-center">
         <h2 className="font-title start__animation text-4xl font-bold text-[#efb810] text-shadow md:text-5xl">
           Esculturas y obras a pedido
